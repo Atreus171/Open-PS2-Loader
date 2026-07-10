9ad94fe33b95bec96648a4bf386e2c39b1b52881
Atreus171 <vitorhugomarquedias@gmail.com>
2026-07-10 12:05:26 -0300

    Fix dialogs.c: remove unused PADEMU_MODE and PADEMU_MODULES_SET entries

--- src/dialogs.c
+++ src/dialogs.c
@@ -567,18 +567,7 @@ struct UIItem diaPadEmuConfig[] = {
     {UI_LABEL, 0, 1, 1, -1, -50, 0, {.label = {NULL, _STR_PADEMU_MODULES}}},
     {UI_SPACER},
     {UI_ENUM, PADCFG_PADEMU_MODULES_LIST, 1, 1, _STR_HINT_PADEMU_MODULES, 0, 0, {.intvalue = {0, 0}}},
-    {UI_BREAK},
-    {UI_BREAK},
-
-    {UI_LABEL, 0, 1, 1, -1, -50, 0, {.label = {NULL, _STR_PADEMU_MODULES_SET}}},
-    {UI_SPACER},
-    {UI_BUTTON, PADCFG_PADEMU_MODULES_SET, 1, 1, _STR_HINT_PADEMU_MODULES_SET, 0, 0, {.label = {NULL, _STR_CONFIGURE}}},
-    {UI_BREAK},
-    {UI_BREAK},
-
-    {UI_LABEL, 0, 1, 1, -1, -50, 0, {.label = {NULL, _STR_PADEMU_MODE}}},
-    {UI_SPACER},
-    {UI_ENUM, PADCFG_PADEMU_MODE, 1, 1, _STR_HINT_PADEMU_MODE, 0, 0, {.intvalue = {1, 1}}},
+{UI_BREAK},
     {UI_BREAK},
 
     {UI_LABEL, 0, 1, 1, -1, -50, 0, {.label = {NULL, _STR_MTAP_ENABLE}}},
