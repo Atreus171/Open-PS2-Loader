3c44e8b84bd2fcba5bf8fb865be2ce9c484691ea
Atreus171 <vitorhugomarquedias@gmail.com>
2026-08-10 20:12:44 -0300

    Fix guigame.c static declarations and SMSUTILS.S export table
    - Restore static qualifier to forward declarations in guigame.c that were
      incorrectly made non-static, causing 'static declaration follows
      non-static declaration' errors.
    - Restore valid IOP export table format in SMSUTILS.S; the previous
      format referenced undefined sms_utils symbol and broke linking.

--- src/guigame.c
+++ src/guigame.c
@@ -71,13 +71,11 @@ static char hexDiscID[15];
 static char configSource[128];
 
 // forward declarations.
-void guiGameLoadGSMConfig(config_set_t *configSet, config_set_t *configGame);
-void guiGameLoadCheatsConfig(config_set_t *configSet, config_set_t *configGame);
+static void guiGameLoadGSMConfig(config_set_t *configSet, config_set_t *configGame);
+static void guiGameLoadCheatsConfig(config_set_t *configSet, config_set_t *configGame);
 #ifdef PADEMU
-void guiGameLoadPadEmuConfig(config_set_t *configSet, config_set_t *configGame);
-void guiGameLoadPadMacroConfig(config_set_t *configSet, config_set_t *configGame);
-void guiGameShowPadEmuConfig(int forceGlobal);
-void guiGameShowPadMacroConfig(int forceGlobal);
+static void guiGameLoadPadEmuConfig(config_set_t *configSet, config_set_t *configGame);
+static void guiGameLoadPadMacroConfig(config_set_t *configSet, config_set_t *configGame);
 #endif
 void guiGameShowVMCMenu(int id, item_list_t *support);
 void guiGameShowGSConfig(void);
@@ -88,8 +86,8 @@ void guiGameRemoveSettings(config_set_t *configSet);
 void guiGameTestSettings(int id, item_list_t *support, config_set_t *configSet);
 void guiGameShowOSDLanguageConfig(int forceGlobal);
 void guiGameSaveOSDLanguageGlobalConfig(config_set_t *configGame);
-int guiGameSaveOSDLanguageGameConfig(config_set_t *configSet, int result);
-void guiGameLoadOSDLanguageConfig(config_set_t *configSet, config_set_t *configGame);
+static int guiGameSaveOSDLanguageGameConfig(config_set_t *configSet, int result);
+static void guiGameLoadOSDLanguageConfig(config_set_t *configSet, config_set_t *configGame);
 
 int guiGameAltStartupNameHandler(char *text, int maxLen)
 {
